5ff88f8f4365b46e0b8a2b7bb74cacd634023fff
Priya Biriya <priyabiriya1707@gmail.com>
2020-10-04 21:31:00 +0530

    [Priya] add . check

--- LineComparision/Program.cs
+++ LineComparision/Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
-
-namespace LineComparision
+namespace LineComparisonComputation
 {
+    class Line
+    {
+        public int X1 { get; set; }
+        public int Y1 { get; set; }
+        public int X2 { get; set; }
+        public int Y2 { get; set; }
+        public double Length()
+        {
+            return Math.Sqrt((X2 - X1) * (X2 - X1) + (Y2 - Y1) * (Y2 - Y1));
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Displaying Welcome to Line Comparison Computation Program on Master Branch!");
+            Console.WriteLine("Welcome to Line Comparison Computation Program");
+            var line1 = new Line { X1 = 0, X2 = 0, Y1 = 1, Y2 = 1 };
+            var line2 = new Line { X1 = 0, X2 = 0, Y1 = -1, Y2 = -1 };
+            Console.WriteLine(line1.Length().Equals(line2.Length()) ? "Lines are Equal" : (line1.Length() > line2.Length() ? "Line 1 is Greater than Line 2" : "Line 2 is Greater than Line1"));
         }
     }
 }
