512c755c9c93646687b2a31a480ab06e046fe968
Priya Biriya <priyabiriya1707@gmail.com>
2020-10-05 08:22:55 +0530

    [priya] add . space

--- LineComparision/Program.cs
+++ LineComparision/Program.cs
@@ -1,22 +1,38 @@
 ﻿using System;
-namespace LineComparisonComputation
+namespace LineComparision
 {
-    class Line
-    {
-        public int X1 { get; set; }
-        public int Y1 { get; set; }
-        public int X2 { get; set; }
-        public int Y2 { get; set; }
-        public double Length()
-        {
-            return Math.Sqrt((X2 - X1) * (X2 - X1) + (Y2 - Y1) * (Y2 - Y1));
-        }
-    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Line Comparison Computation Program");
+            Console.WriteLine("===========================================");
+            Console.WriteLine("Welcome to the Line Comparision Computation");
+            Console.WriteLine("===========================================");
+            int[] firstPoint = new int[2];
+            int[] secondPoint = new int[2];
+            StorePoints(ref firstPoint, ref secondPoint);
+            double lineLength;
+            lineLength = FindLength(firstPoint, secondPoint);
+            Console.WriteLine("Length of the line is " + lineLength);
+            return;
+        }
+        static void StorePoints(ref int[] p1, ref int[] p2)
+        {
+            Console.WriteLine("Enter X-Coordinate of 1st point");
+            p1[0] = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Y-Coordinate of 1st point");
+            p1[1] = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Enter X-Coordinate of 2nd point");
+            p2[0] = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Y-Coordinate of 2nd point");
+            p2[1] = Int32.Parse(Console.ReadLine());
+            return;
+        }
+        static double FindLength(int[] p1, int[] p2)
+        {
+            double length;
+            length = Math.Sqrt((p2[0] - p1[0]) ^ 2 + (p2[1] - p1[1]) ^ 2);
+            return Math.Round(length, 2);
         }
     }
 }
